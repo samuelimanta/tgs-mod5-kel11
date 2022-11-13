bfb4e04be96eb2b09957decc7dfac642993d133d
anharID <63109799+anharID@users.noreply.github.com>
2022-11-13 21:53:00 +0700

    get request

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import axios from "axios";
+
+const BASE_API_URL = `https://jsonplaceholder.typicode.com`;
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const getPost = async () => {
+      try {
+        let response = await axios.get(`${BASE_API_URL}/posts`);
+        setPosts(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getPost();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Daftar Postingan</h1>
+      <p>by Kelompok 11</p>
+      {posts.map((post) => {
+        return (
+          <Card className="mb-3">
+            <Card.Body key={post.id}>
+              <Card.Title>{post.title}</Card.Title>
+              <Card.Text>{post.body}</Card.Text>
+              <Button variant="danger">Delete</Button>
+            </Card.Body>
+          </Card>
+        );
+      })}
     </div>
   );
 }
